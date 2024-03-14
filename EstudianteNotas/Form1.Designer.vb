87c355763a3d739c3779b2bcfa0a11026f8a083a
= <=>
2024-03-13 20:10:59 -0600

    Subida

---
+++ EstudianteNotas/Form1.Designer.vb
@@ -0,0 +1,264 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.dataGridNotas = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtNombre = New System.Windows.Forms.TextBox()
+        Me.txtApellido = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtparcial1 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtparcial2 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtPromedio = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnGuardar = New System.Windows.Forms.Button()
+        CType(Me.dataGridNotas, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(279, 9)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(336, 25)
+        Me.label1.TabIndex = 0
+        Me.label1.Text = "Notas de estudiante Informatica III"
+        '
+        'dataGridNotas
+        '
+        Me.dataGridNotas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataGridNotas.Location = New System.Drawing.Point(13, 40)
+        Me.dataGridNotas.Name = "dataGridNotas"
+        Me.dataGridNotas.Size = New System.Drawing.Size(855, 160)
+        Me.dataGridNotas.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label2.Location = New System.Drawing.Point(63, 235)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "ID:"
+        '
+        'txtID
+        '
+        Me.txtID.Location = New System.Drawing.Point(100, 235)
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(111, 20)
+        Me.txtID.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label3.Location = New System.Drawing.Point(30, 281)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Nombre:"
+        '
+        'txtNombre
+        '
+        Me.txtNombre.Location = New System.Drawing.Point(100, 284)
+        Me.txtNombre.Name = "txtNombre"
+        Me.txtNombre.Size = New System.Drawing.Size(111, 20)
+        Me.txtNombre.TabIndex = 3
+        '
+        'txtApellido
+        '
+        Me.txtApellido.Location = New System.Drawing.Point(100, 330)
+        Me.txtApellido.Name = "txtApellido"
+        Me.txtApellido.Size = New System.Drawing.Size(111, 20)
+        Me.txtApellido.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label4.Location = New System.Drawing.Point(26, 328)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(73, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Apellido:"
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(30, 400)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(94, 35)
+        Me.btnBuscar.TabIndex = 6
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(144, 400)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(94, 35)
+        Me.btnSalir.TabIndex = 7
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(724, 203)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(51, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Notas"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label6.Location = New System.Drawing.Point(617, 235)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(75, 20)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "I Parcial:"
+        '
+        'txtparcial1
+        '
+        Me.txtparcial1.Location = New System.Drawing.Point(698, 235)
+        Me.txtparcial1.Name = "txtparcial1"
+        Me.txtparcial1.Size = New System.Drawing.Size(111, 20)
+        Me.txtparcial1.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label7.Location = New System.Drawing.Point(628, 281)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(79, 20)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "II Parcial:"
+        '
+        'txtparcial2
+        '
+        Me.txtparcial2.Location = New System.Drawing.Point(708, 283)
+        Me.txtparcial2.Name = "txtparcial2"
+        Me.txtparcial2.Size = New System.Drawing.Size(111, 20)
+        Me.txtparcial2.TabIndex = 3
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label8.Location = New System.Drawing.Point(624, 328)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(85, 20)
+        Me.Label8.TabIndex = 4
+        Me.Label8.Text = "Promedio:"
+        '
+        'txtPromedio
+        '
+        Me.txtPromedio.Location = New System.Drawing.Point(708, 330)
+        Me.txtPromedio.Name = "txtPromedio"
+        Me.txtPromedio.Size = New System.Drawing.Size(111, 20)
+        Me.txtPromedio.TabIndex = 5
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(632, 398)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(94, 35)
+        Me.btnCalcular.TabIndex = 6
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnGuardar
+        '
+        Me.btnGuardar.Location = New System.Drawing.Point(745, 398)
+        Me.btnGuardar.Name = "btnGuardar"
+        Me.btnGuardar.Size = New System.Drawing.Size(94, 35)
+        Me.btnGuardar.TabIndex = 7
+        Me.btnGuardar.Text = "Guardar"
+        Me.btnGuardar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(938, 445)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnGuardar)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.txtPromedio)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtApellido)
+        Me.Controls.Add(Me.txtparcial2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtNombre)
+        Me.Controls.Add(Me.txtparcial1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.dataGridNotas)
+        Me.Controls.Add(Me.label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dataGridNotas, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents label1 As Label
+    Friend WithEvents dataGridNotas As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNombre As TextBox
+    Friend WithEvents txtApellido As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtparcial1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtparcial2 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtPromedio As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnGuardar As Button
+End Class
---
+++ EstudianteNotas/Form1.vb
@@ -0,0 +1,130 @@
+﻿Imports System.Math
+Public Class Form1
+
+
+
+    Public conex As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=estudiantes.accdb")
+    Public com As New OleDb.OleDbCommand
+
+    Private Sub ConectarDB()
+        Try
+            conex.Open()
+            com.Connection = conex
+            com.CommandType = CommandType.Text
+            Console.WriteLine("Conexion con la base de datos correcta.")
+        Catch ex As Exception
+
+            If Err.Number = 5 Then
+                MsgBox("Error al conectar la base de datos." & Err.Description)
+            End If
+
+        End Try
+    End Sub
+
+
+    Private Sub refrescarDatos()
+
+        Dim sqlActualizado As String = "SELECT * FROM Notas"
+        Dim adapt As New OleDb.OleDbDataAdapter(sqlActualizado, conex)
+        Dim obtenerDatos As New DataSet
+        adapt.Fill(obtenerDatos, "Id")
+
+        dataGridNotas.DataSource = obtenerDatos
+        dataGridNotas.DataMember = "Id"
+
+
+    End Sub
+
+
+    Private Sub btnGuardar_Click(sender As Object, e As EventArgs) Handles btnGuardar.Click
+        Dim sql, ID, Nombre, Apellido, nota1, nota2, promedio As String
+
+
+        ID = txtID.Text
+        Nombre = txtNombre.Text
+        Apellido = txtApellido.Text
+        nota1 = txtparcial1.Text
+        nota2 = txtparcial2.Text
+        promedio = txtPromedio.Text
+
+        sql = String.Format("INSERT INTO Notas(Id,nombre,apellido,Iparcial,IIparcial,promedio) VALUES({0},'{1}','{2}',{3},{4},{5})", ID, Nombre, Apellido, nota1, nota2, promedio)
+
+
+        com.CommandText = sql
+
+        Try
+            com.ExecuteNonQuery()
+            refrescarDatos()
+            MsgBox("Registro ID: " & ID & " guardado correctamente.")
+
+        Catch ex As Exception
+            MsgBox(Err.Description)
+        End Try
+
+        Console.WriteLine(sql)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ConectarDB()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim idABuscar As String = txtID.Text
+        If idABuscar = "" Then
+            MsgBox("¡Se necesita un ID!")
+            Return
+        End If
+        Dim sqlActualizado As String = "SELECT * FROM Notas WHERE Id = " & txtID.Text & ""
+        Dim adapt As New OleDb.OleDbDataAdapter(sqlActualizado, conex)
+        Dim ds As New DataSet
+        adapt.Fill(ds)
+
+        If ds.Tables(0).Rows.Count = 0 Then
+            MsgBox("No se encuentra ningun registro en la base de datos")
+            Return
+
+        End If
+
+        Dim dato As DataRow = ds.Tables(0).Rows(0)
+
+
+        txtID.Text = dato("Id").ToString()
+        txtNombre.Text = dato("nombre").ToString()
+        txtApellido.Text = dato("apellido").ToString()
+        txtparcial1.Text = dato("Iparcial").ToString()
+        txtparcial2.Text = dato("IIparcial").ToString()
+        txtPromedio.Text = dato("promedio").ToString()
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        conex.Close()
+        Application.Exit()
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim prom, nota1, nota2 As Integer
+        nota1 = Val(txtparcial1.Text)
+        nota2 = Val(txtparcial2.Text)
+
+        prom = Round((nota1 + nota2)) / 2
+
+        txtPromedio.Text = prom.ToString()
+
+    End Sub
+
+
+    'Oscar Fabian Lopez Raudales
+End Class
